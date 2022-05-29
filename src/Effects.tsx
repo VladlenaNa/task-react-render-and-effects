1cfdee10168fbd3e519e4193cc3fdff87e2dfdba
Vlada <vladaa1998@yandex.ru>
2022-05-30 01:33:16 +0500

    Задание по реакту эффекты

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,20 @@
+import { useCallback, useEffect, useState } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
-
+//полдня читать статьи, чтобы написать 3 строчки кода - это работа программиста...
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [msg, setMsg] = useState('-1');
+    const [sourceId, setSourceId] = useState(props.sourceId);
+    const callback = useCallback((msg) => {
+        setMsg(msg);
+    }, []);
+    useEffect(() => {
+        console.log(props.sourceId, sourceId)
+        setSourceId(props.sourceId);
+        subscribe(props.sourceId, callback);
+        setMsg('-1')
+        return ()=>{
+            unsubscribe(props.sourceId, callback)
+        }
+    }, [props.sourceId]);
+    return <div>{sourceId}: {msg}</div>;
 }
